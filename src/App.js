f68c47264440b8ef8a426614581d1dd623c13558
Taylor H <tttahobbs@gmail.com>
2023-07-26 13:50:49 -0500

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todo, setTodo] = useState([]);
+  const [title, setTitle] = useState("");
+
+  function handleAddTodo(todo) {
+    if (!todo) return;
+    setTodo((todos) => [...todos, todo]);
+  }
+
+  function handleDeleteTodo(id) {
+    setTodo((todos) => todos.filter((item) => item.id !== id));
+  }
+
+  function handleToggle(id) {
+    setTodo((todos) =>
+      todos.map((item) =>
+        item.id === id ? { ...item, completed: !item.completed } : item
+      )
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content-container">
+      <Welcome />
+      <NewToDoItem
+        title={title}
+        setTitle={setTitle}
+        todo={todo}
+        addTodo={handleAddTodo}
+      />
+      <DisplayToDo
+        todo={todo}
+        deleteTodo={handleDeleteTodo}
+        handleToggle={handleToggle}
+      />
     </div>
   );
 }
 
-export default App;
+function Welcome() {
+  return (
+    <div className="header">
+      <h1>TODO LIST</h1>
+    </div>
+  );
+}
+
+function NewToDoItem({ title, setTitle, addTodo, todo }) {
+  function handleTodoSubmit(e) {
+    e.preventDefault();
+
+    if (!title) return;
+
+    const newTodo = { title, completed: false, id: Date.now() };
+
+    addTodo(newTodo);
+    setTitle("");
+  }
+  return (
+    <div className="form">
+      <form onSubmit={handleTodoSubmit}>
+        <div className="form-format">
+          <button className="button">+</button>
+          <input
+            type="text"
+            placeholder="New Todo Item"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></input>
+        </div>
+      </form>
+      <form></form>
+    </div>
+  );
+}
+
+function DisplayToDo({ todo, deleteTodo, handleToggle }) {
+  /*const [sortBy, setSortBy] = useState("NtO");
+
+  let sorted;
+
+  if (sortBy === "NtO") sorted = todo;
+
+  if (sortBy === "AtZ")
+    sorted = todo.slice().sort((a, b) => a.title.localeCompare(b.title));
+
+  if (sortBy === "packed")
+    sorted = todo
+      .slice()
+      .sort((a, b) => Number(a.completed) - Number(b.completed)); */
+  return (
+    <div className="todo-list">
+      {/*<select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+        <option value="NtO">Sort by Newest to Oldest</option>
+        <option value="OtN">Sort by Oldest to Newest</option>
+        <option value="AtZ">Sort by A-Z</option>
+        <option value="ZtA">Sort by Z-A</option>
+        <option value="completed">Sort by Completed</option>
+        <option value="incompleted">Sort by Incompleted</option>
+  </select> */}
+      {todo.map((title) => (
+        <Todo
+          title={title}
+          key={title.id}
+          deleteTodo={deleteTodo}
+          handleToggle={handleToggle}
+        />
+      ))}
+    </div>
+  );
+}
+
+function Todo({ title, handleToggle, deleteTodo }) {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" onInput={() => handleToggle(title.id)} />
+      <h3
+        style={
+          title.completed
+            ? { textDecoration: "line-through", color: "grey" }
+            : {}
+        }
+      >
+        {title.title}
+      </h3>
+      <button className="delete-button" onClick={() => deleteTodo(title.id)}>
+        ❌
+      </button>
+    </div>
+  );
+}
